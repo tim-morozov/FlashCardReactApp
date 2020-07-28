540018039a79fda6b22d18bdb9c88edfbf733fb8
tim-morozov <tim.morozov@gmail.com>
2020-07-28 11:36:22 -0500

    Added label to select and style

--- react-app/flashcardapp/src/components/Collection.js
+++ react-app/flashcardapp/src/components/Collection.js
@@ -6,11 +6,14 @@ class Collection extends Component {
     
     render() {
         return (
+            <div>
+            <label style={{paddingRight: '10px'}}>Collections</label>
             <select htmlFor='Collections'>
             {this.props.collections.map((collection) => (
                 <option value={collection.id} key={collection.id}>{collection.title}</option>
             ))}
             </select>
+            </div>
         );
     }
 
